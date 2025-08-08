1fa0279d8eaf4c08286bdf6727c6993a27ac6029
Hairo <hasnaouinidal308@gmail.com>
2025-08-08 15:02:34 +0100

    this is me and i finsih th nav bar and start in footer !

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask , render_template
 
 app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
 
-@app.route('/')
+@app.route('/nav')
 def index(): 
-    return 'hello world !'
+    return render_template('nav.html')
 
 if __name__ == '__main__': 
     app.run(debug=True)
