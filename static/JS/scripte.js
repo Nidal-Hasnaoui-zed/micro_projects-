ce5e2465337779e80ca9a8e8cc45e4eadefc25a5
Hairo <hasnaouinidal308@gmail.com>
2025-08-08 21:24:37 +0100

    this is me bro !

--- static/JS/scripte.js
+++ static/JS/scripte.js
@@ -1,1 +1,26 @@
-console.log('hey my name is nidal hasnaoui and i\'m backend dev !')
+
+let task = document.getElementById('input-box'); 
+let container = document.getElementById('list-container');
+
+function addTask(){
+    if(task.value.trim() === ''){
+        alert('plz enter a task !')
+    }else {
+        let li = document.createElement('li'); 
+        li.textContent = task.value ; 
+        container.appendChild(li); 
+
+        let span = document.createElement('span'); 
+        span.innerHTML = '\u00d7' ;
+        li.appendChild(span); 
+        
+        task.value = ''; 
+    }
+}
+ container.addEventListener('click' , function(e){
+        if(e.target.tagName === 'LI'){
+            e.target.classList.toggle('checked'); 
+        }else if(e.target.tagName === 'SPAN'){
+            e.target.parentElement.remove();
+        }
+    })
