9bcea71b6c77ce1008b261d36fa9888c140972fa
Hairo <hasnaouinidal308@gmail.com>
2025-08-08 15:13:03 +0100

    this is me bro and i wnna you !

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask , render_template
 
 app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
 
-@app.route('/footer')
+@app.route('/')
 def index(): 
-    return render_template('footer.html')
+    return render_template('index.html')
+
+@app.route('/todo')
+def todo_app(): 
+    return render_template('todo.html');
 
 if __name__ == '__main__': 
     app.run(debug=True)
